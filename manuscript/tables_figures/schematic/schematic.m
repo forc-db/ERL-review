68d9dfdcfa1acd5ec43996c65d3f64c5dfd5198e
Kristina Anderson-Teixeira <teixeirak@si.edu>
2021-01-15 09:03:33 -0500

    work on schematic
    #61

--- manuscript/tables_figures/schematic/schematic.m
+++ manuscript/tables_figures/schematic/schematic.m
@@ -30,21 +30,31 @@ biome_names={'tropical', 'temperate', 'boreal'};
 %FLUXES
 % 1. by age
 %for stacked plot
-R_auto = 2* log10(age);
-NPP = R_auto;
+NEP=2*log10(age)-.037*(age-1); %placeholder
+GPP= 4* log10(age)+NEP;
+R_auto = 0.5*GPP;
+NPP = GPP-R_auto;
+R_het=GPP-R_auto;
 
-fluxes = [R_auto; NPP]; %matrix with all fluxes for stacked plot
-flux_names = {'R_{auto}', 'NPP'};
 
-%others
-GPP = sum(fluxes,1);
+%categorize by "in" (GPP components, including NEP) and "out" (Reco components)
+in_fluxes = [R_auto; NPP; NEP]; %matrix with all fluxes for stacked plot
+in_flux_names = {'R_{auto}', 'NPP', 'NEP'};
+out_fluxes = [R_auto; R_het];
+out_flux_names = {'R_{auto}', 'R_{het}'};
+
+%sum in and out fluxes
+in_sum = sum(in_fluxes,1);
+out_sum=sum(out_fluxes,1);
 
 % 2. mature fluxes
 Flux_ratio_temp_trop=0.5;
-Flux_ratio_boreaal_trop=0.25;
+Flux_ratio_boreal_trop=0.25;
 
-%for stacked plot
-mature_fluxes = [fluxes(:,end)'; Flux_ratio_temp_trop*fluxes(:,end)' ; Flux_ratio_boreaal_trop*fluxes(:,end)'];
+%for grouped stacked plot
+mature_fluxes=0*ones(3,2,size(in_fluxes,1)); %empty 3D matrix (Group=biome, Stack=bar(GPP vs Reco), StackElement=component)
+mature_fluxes(:,1,1:size(in_fluxes,1))=[in_fluxes(:,end)'; Flux_ratio_temp_trop*in_fluxes(:,end)' ; Flux_ratio_boreal_trop*in_fluxes(:,end)']; %components of GPP
+mature_fluxes(:,2,1:size(out_fluxes,1))=[out_fluxes(:,end)'; Flux_ratio_temp_trop*out_fluxes(:,end)' ; Flux_ratio_boreal_trop*out_fluxes(:,end)'];
 
 %others
 
@@ -71,41 +81,43 @@ mature_stocks = [stocks(:,end)'; Stock_ratio_temp_trop*stocks(:,end)' ; Stock_ra
 %% ~~~~~~~PLOTTING~~~~~~~~~~
 
 figure (1)
-subplot (2,3,1:2)
-%flux plot:
-h=area (age, fluxes'); %, 'LineStyle','-'); 
-hold on;
-plot(age, GPP, '-g', 'LineWidth', 3);
-t = title('age trends'); 
-ylabel ('fluxes');
-set(gca, 'XTick', []); %ticks off
-set(gca, 'YTick', []); %ticks off
 
-
-subplot (2,3,3)
-%bar(mature_fluxes, 'stacked');
-plotBarStackGroups(mature_fluxes, biome_names);
+subplot (2,3,1)
+%stack as components of GPP and Reco, outline those.
+plotBarStackGroups(mature_fluxes, biome_names); 
 t = title('biome differences');
-legend (flux_names);
+legend ([in_flux_names, out_flux_names]);
+ylabel ('C fluxes (Mg C ha^{-1} yr^{-1})');
 set(gca, 'XTick', []); %ticks off
 set(gca, 'YTick', []); %ticks off
 
-subplot (2,3,4:5)
+subplot (2,3,2:3)
+%flux plot:
+h=area (age, in_fluxes'); %, 'LineStyle','-'); 
+hold on;
+plot(age, in_sum, '-b', 'LineWidth', 3); hold on;
+plot(age, out_sum, '-r', 'LineWidth', 3);
+t = title('age trends'); 
+set(gca, 'XTick', []); %ticks off
+set(gca, 'YTick', []); %ticks off
+
+subplot (2,3,4)
+bar(mature_stocks, 'stacked');
+legend (stock_names);
+xlabel ('biome');
+ylabel ('C  stocks (Mg C ha^{-1})')
+set(gca, 'XTick', []); %ticks off
+set(gca, 'YTick', []); %ticks off
+
+subplot (2,3,5:6)
 %flux plot:
 h=area (age, stocks'); %, 'LineStyle','-'); 
 hold on;
 plot(age, biomass, '-k', 'LineWidth', 3);
-ylabel ('stocks');
 xlabel ('stand age');
 set(gca, 'XTick', []); %ticks off
 set(gca, 'YTick', []); %ticks off
 
-subplot (2,3,6)
-bar(mature_stocks, 'stacked');
-legend (stock_names);
-xlabel ('biome');
-set(gca, 'XTick', []); %ticks off
-set(gca, 'YTick', []); %ticks off
 
 
 %% ~~~~~~~ SAVE FIGURE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
